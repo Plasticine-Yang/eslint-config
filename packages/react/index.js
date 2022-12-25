b8768349cedf110488df58ffd326bf891b78f381
plasticine9750 <975036719@qq.com>
2022-12-25 18:28:26 +0800

    feat(react): @plasticine/eslint-config-react

---
+++ packages/react/index.js
@@ -0,0 +1,21 @@
+/** @type { import('eslint').ESLint.ConfigData } */
+module.exports = {
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    '@plasticine/eslint-config-typescript',
+  ],
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+  },
+
+  rules: {
+    // react
+    'react/prop-types': 'off',
+  },
+}
